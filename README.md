8af2125b15c5399a37f796ba298452d01f3f83fc
AlexanderJoker <alexandertinoco12349@gmail.com>
2021-11-19 00:57:32 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Programacion Movil 1 - Examen 2 - Grupo 1
+<h1>Intengrantes </h1>
+<h3>Hector Alberto Soriano Gonzalez     201810060177</h3>
+<h3>Blanca Rosa Cruz Corea			    201810060452</h3>
+<h3>Yeltsin Horacio Baquedano Martínez 	201820110108</h3>
+<h3>Karol Stephany Palma Ventura		201910060112</h3>
+<h3>Miriam Ariel Mondragón Espinoza		201910060126</h3>
+<h3>Óscar Edgardo Laínez Cárcamo		201910060511</h3>
+<hr>
+
+
---
+++ app/src/main/java/com/example/pm1e2grupo4/RestApiMethods.java
@@ -0,0 +1,10 @@
+package com.example.pm1e2grupo4;
+
+public class RestApiMethods {
+    public static final String server = "http://3.128.217.135:1880";
+    public static final String ApiGetUrl = server+"/api/contactos/lista";
+    public static final String ApiGetID = server+"/api/contacto/informacion/";
+    public static final String ApiCreateUrl = server+"/api/contacto/create";
+    public static final String ApiUpdateUrl = server+"/api/contacto/update";
+    public static final String ApiDeleteUrl = server+"/api/contacto/delete/";
+}
